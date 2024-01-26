332dd424a0ff157bef2bd9498cd5bd93b3bf3716
Janssen Batista <batistajanssen.dev@gmail.com>
2024-01-26 12:59:55 -0300

    refactor: change no message container and result container size

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ const resultMessageContainer = document.querySelector(
 );
 
 const showResultMessageContainer = () => {
-  resultMessageContainer.style.display = "block";
+  resultMessageContainer.style.display = "flex";
   noMessageContainer.style.display = "none";
 };
 
